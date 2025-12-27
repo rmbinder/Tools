41424e3d1e2e0af06c798f9636b72fa29e640e66
rmbinder <r-m-b@gmx.net>
2025-12-27 20:54:41 +0100

    fix error with open_basedir, #32

--- system/common_function.php
+++ system/common_function.php
@@ -16,20 +16,18 @@ use Plugins\Tools\classes\Config\ConfigTable;
 require_once(__DIR__ . '/../../../system/common.php');
 
 $folders = explode('/', $_SERVER['SCRIPT_FILENAME']);
-while (array_search(substr(FOLDER_PLUGINS, 1), $folders))
-{
+while (array_search(substr(FOLDER_PLUGINS, 1), $folders)) {
     array_shift($folders);
 }
 array_shift($folders);
+array_pop($folders);
 
-if(!defined('PLUGIN_FOLDER'))
-{
-    define('PLUGIN_FOLDER', '/'.$folders[0]);
+if (! defined('PLUGIN_FOLDER')) {
+    define('PLUGIN_FOLDER', '/' . $folders[0]);
 }
 
-if (!defined('PLUGIN_SUBFOLDER') && is_file(ADMIDIO_PATH . FOLDER_PLUGINS . PLUGIN_FOLDER .'/'.$folders[1].'/'.$folders[1].'.php') )
-{
-    define('PLUGIN_SUBFOLDER', '/'.$folders[1]);
+if (! defined('PLUGIN_SUBFOLDER') && isset($folders[1]) && is_file(ADMIDIO_PATH . FOLDER_PLUGINS . PLUGIN_FOLDER . '/' . $folders[1] . '/' . $folders[1] . '.php')) {
+    define('PLUGIN_SUBFOLDER', '/' . $folders[1]);
 }
 unset($folders);
 
@@ -39,26 +37,28 @@ spl_autoload_register('myAutoloader');
  * Mein Autoloader
  * Script aus dem Netz
  * https://www.marcosimbuerger.ch/tech-blog/php-autoloader.html
- * @param   string  $className   Die übergebene Klasse
- * @return  string  Der überprüfte Klassenname
+ *
+ * @param string $className    Die übergebene Klasse
+ * @return string Der überprüfte Klassenname
  */
-function myAutoloader($className) {
+function myAutoloader($className)
+{
     // Projekt spezifischer Namespace-Prefix.
     $prefix = 'Plugins\\';
-    
+
     // Base-Directory für den Namespace-Prefix.
     $baseDir = __DIR__ . '/../../';
-    
+
     // Check, ob die Klasse den Namespace-Prefix verwendet.
     $len = strlen($prefix);
-    
+
     if (strncmp($prefix, $className, $len) !== 0) {
         // Wenn der Namespace-Prefix nicht verwendet wird, wird abgebrochen.
         return;
     }
     // Den relativen Klassennamen ermitteln.
     $relativeClassName = substr($className, $len);
-    
+
     // Den Namespace-Präfix mit dem Base-Directory ergänzen,
     // Namespace-Trennzeichen durch Verzeichnis-Trennzeichen im relativen Klassennamen ersetzen,
     // .php anhängen.
